f5032ec16f3479c74c3f003373b940a6d528de4d
FrancisBFTC <bftcorporations@gmail.com>
2025-02-11 02:21:25 -0300

    test: Testando o código do sistema decimal

--- teste14.asm
+++ teste14.asm
@@ -1,4 +1,12 @@
 define num1 $20
 define num2 20
-adc (num1),Y
-adc (num2,X)
+; codigo comentado --------
+;adc (num1),Y
+;adc (num2,X)
+;adc ($10),Y
+;adc (10,X)
+; -------------------------
+adc #num1
+adc num1
+adc #num2
+adc num2
