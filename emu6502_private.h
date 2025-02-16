2b2f0dfdf3d9d1ad3da8f9f035867fa850a1435f
FrancisBFTC <bftcorporations@gmail.com>
2025-02-16 03:21:39 -0300

    fix: Corrige versão do projeto

--- emu6502_private.h
+++ emu6502_private.h
@@ -7,17 +7,17 @@
 /* VERSION DEFINITIONS */
 #define VER_STRING	"0.7.0.27"
 #define VER_MAJOR	0
-#define VER_MINOR	7
+#define VER_MINOR	8
 #define VER_RELEASE	0
 #define VER_BUILD	27
 #define COMPANY_NAME	"KiddieOS.Community"
-#define FILE_VERSION	"0.7.0.27"
+#define FILE_VERSION	"0.8.0.27"
 #define FILE_DESCRIPTION	"6502 Assembler and Emulator for console"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"emu6502"
-#define PRODUCT_VERSION	"0.7.0.27"
+#define PRODUCT_VERSION	"0.8.0.27"
 
 #endif /*EMU6502_PRIVATE_H*/
