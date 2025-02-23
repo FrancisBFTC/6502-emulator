4556ce8219dead0a52804919e96e221823e90363
FrancisBFTC <bftcorporations@gmail.com>
2025-02-23 03:44:39 -0300

    feat: Adiciona ferramenta de testes automatizados via Script

--- emu6502_private.h
+++ emu6502_private.h
@@ -5,19 +5,19 @@
 #define EMU6502_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.9.0.91"
+#define VER_STRING	"0.9.0.93"
 #define VER_MAJOR	0
 #define VER_MINOR	9
 #define VER_RELEASE	0
-#define VER_BUILD	91
+#define VER_BUILD	93
 #define COMPANY_NAME	"Wenderson Anjos (KiddieOS.Community)"
-#define FILE_VERSION	"0.9.0.91"
+#define FILE_VERSION	"0.9.0.93"
 #define FILE_DESCRIPTION	"6502 Assembler and Emulator for console"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"emu6502"
-#define PRODUCT_VERSION	"0.9.0.91"
+#define PRODUCT_VERSION	"0.9.0.93"
 
 #endif /*EMU6502_PRIVATE_H*/
--- vector.asm
+++ vector.asm
@@ -1,11 +1,8 @@
-define str1 String1
-define str2 String2
-
-Vector:
-	dcb str1, <str2
-	
 String1:
 	dcb $48, $65, $6C, $6F
 
 String2:
 	dcb $4F, $6C, $61
+	
+Vector:
+	dcb String1, <String1
