6e3c5b66cf319767164908e70a6c4ace60a98130
FrancisBFTC <bftcorporations@gmail.com>
2025-03-03 01:21:56 -0300

    feat: Testes de instruções de registros e flags

--- tools/test.bat
+++ tools/test.bat
@@ -1,3 +1,3 @@
 echo OFF
-tester adc.tt and.tt asl.tt bit.tt cmp.tt cpx.tt cpy.tt dec.tt eor.tt inc.tt lda.tt ldx.tt ldy.tt lsr.tt ora.tt rol.tt ror.tt sbc.tt sta.tt stx.tt sty.tt brk.tt nop.tt
+tester adc.tt and.tt asl.tt bit.tt cmp.tt cpx.tt cpy.tt dec.tt eor.tt inc.tt lda.tt ldx.tt ldy.tt lsr.tt ora.tt rol.tt ror.tt sbc.tt sta.tt stx.tt sty.tt brk.tt nop.tt flags.tt regs.tt
 echo ON
